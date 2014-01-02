b6bc4caa005b338f0083fd07e486a80f0741b4f4
Daniel Jallov <danieljallov@gmail.com>
2014-01-02 22:13:33 +0100

    With pg

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
-require 'json'
+require 'json' 
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
 
