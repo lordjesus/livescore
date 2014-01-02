46a01d3dd1a36dfc897c11b3c4e56423f088319c
Daniel Jallov <danieljallov@gmail.com>
2014-01-02 22:28:36 +0100

    Rename

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ require 'json'
 DataMapper.setup(:default, ENV['DATABASE_URL']) # || "sqlite3://#{Dir.pwd}/development.db")
 
 class Player
-	include DataMapper::Resource
+	include DataMapper::Resource 
 
 	property :id, 		Serial
 	property :name, 	String
