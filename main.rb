9966eedb3748452d0a41a100527fc04652fd4b4a
Daniel Jallov <danieljallov@gmail.com>
2014-02-25 22:37:17 +0100

    Changed password

--- main.rb
+++ main.rb
@@ -78,7 +78,7 @@ helpers do
 
   def authorized?
     @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'admin']
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'hjorring_open']
   end
 end
 
